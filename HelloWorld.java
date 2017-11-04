0439b305ecd5b09b0fd0be36019723bea3fec2d1
Jay <33330029+bigrepo@users.noreply.github.com>
2017-11-04 11:46:11 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ class HelloWorld
 {
   public static void main(String[] S)
    {
-     System.out.print("Hello World");
+     System.out.print("Hello World")
       //This java program was written on 11/02/17
      // Java program is writtn after almost 8 years :)
    }
