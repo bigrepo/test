dc6dbac1d8345c04b01390e120755be3d5805427
Jay <jaydesh@yahoo.com>
2017-11-03 20:56:08 -0400

    Checking in 4

--- JavaProgram.java
+++ JavaProgram.java
@@ -4,5 +4,7 @@ class JavaProgram
   public static void main(String[] S)
    {
      System.out.print("This is my Java Program for today commited and auto compiled");
+
+     System.out.print("Yahooooooooooooooooo");
    }
 }
